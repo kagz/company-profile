767f5973001ba8a82404ca95f262a41b23076dc8
kagwi <kagwiandrew@gmail.com>
2020-10-07 18:32:38 +0300

    sorted the header

--- docroot/js/main.js
+++ docroot/js/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+	const closeOverlay = document.getElementById('close-overlay');
+	const navbarToggler = document.querySelector('.navbar-toggler');
 	const searchToggleButton = document.getElementById('main-header-search-toggle-button');
 	const searchContainer = document.getElementById('main-header-search-container');
+
+	navbarToggler.addEventListener('click', () => {
+		closeOverlay.classList.add('active');
+	});
+
+	closeOverlay.addEventListener('click', e => {
+		e.target.classList.remove('active');
+		$('.navbar-collapse').collapse('hide');
+	});
 
 	searchToggleButton.addEventListener('click', () => {
 		searchContainer.classList.add('active');
